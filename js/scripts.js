97217c58bfa29a311a1aa946cb6c887517169a27
Dawid G <dawid.grudziecki@gmail.com>
2017-11-07 02:11:26 +0100

    3 files in what is all about

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+var variable = 'test';
+
+console.log(variable);
+
+var firstSentence = 'I like a song that is called "Only you".';
+var secondSentence = "I love a song that is called 'Only you'.";
+
+console.log(firstSentence);
+console.log(secondSentence);
+
+var test = ['HTML', 5, 'World', 9.99, ['Lorem', 'Ipsum', 2]];
+
+var foo = test[2];
+console.log(foo);
+
+console.log(person);
+var person;
+console.log(person);
